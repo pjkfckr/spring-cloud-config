9398e0139a03c30cc2ead58dbc6418e8843628cb
Sangyeong <pjkfckr@gmail.com>
2023-03-07 11:36:36 +0900

    client and server

---
+++ config-server/src/test/java/com/showing/config/server/ConfigServerTest.java
@@ -0,0 +1,12 @@
+package com.showing.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
